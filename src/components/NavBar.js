d11caeaf55049f187c4080d3f3c2d6b29b0d4a70
bek <bekrye@gmail.com>
2023-09-18 17:46:39 +0200

    Add NavBar and css styles

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css"
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="50" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto text-start">
+            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
+            <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
+            <Nav.Link><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
