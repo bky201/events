ecafcacc25284c204a24ac60118fa251952079d6
bek <bekrye@gmail.com>
2023-09-19 19:11:56 +0200

    Create Sign in status and refresh access token

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-      <App />
-    </Router>
+  <Router>
+  <CurrentUserProvider>
+    <App />
+  </CurrentUserProvider>
+</Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
